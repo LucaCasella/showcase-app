19b9f4155eea68347a9ed4c39b9187baaa9e5b90
Manuel9703 <97989541+Manuel9703@users.noreply.github.com>
2024-01-07 02:17:33 +0100

    add new route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\GuestFormController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ShowPriceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,16 +18,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/backoffice', function () {
+    return view('backoffice.dashboard');
+})->middleware(['auth', 'verified'])->name('backoffice');
+
+Route::post('/guest-form-submit',[GuestFormController::class, 'store'])->name('guest-form');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::middleware('guestVerified')->group(function (){
+    Route::get('/price', [ShowPriceController::class, 'show'])->name('price-page');
+});
+
 
 require __DIR__.'/auth.php';
