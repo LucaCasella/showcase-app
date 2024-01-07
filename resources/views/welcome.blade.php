ca69c691173f385a082907a8fb6efad06a5a9094
Manuel9703 <97989541+Manuel9703@users.noreply.github.com>
2024-01-07 02:15:49 +0100

    add update blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,7 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                        <a href="{{ url('/backoffice') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Back-office</a>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
@@ -121,7 +121,7 @@
                             </a>
                         </div>
                     </div>
-
+                    <x-guest-form-verifier />
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
