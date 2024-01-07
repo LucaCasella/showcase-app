861cfb5ae239e316695fb3d2b8991130080a6370
Manuel9703 <97989541+Manuel9703@users.noreply.github.com>
2024-01-07 16:03:27 +0100

    Add new route to show logs

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\GuestFormController;
+use App\Http\Controllers\LogController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ShowPriceController;
 use Illuminate\Support\Facades\Route;
@@ -36,5 +37,6 @@ Route::middleware('guestVerified')->group(function (){
     Route::get('/price', [ShowPriceController::class, 'show'])->name('price-page');
 });
 
+Route::get('/logs', [LogController::class, 'showLogs']);
 
 require __DIR__.'/auth.php';
