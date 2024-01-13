4f92f13decc8caeb06b106986d60978079d73854
Luca <filoluca92@gmail.com>
2024-01-13 18:19:18 +0100

    created multilanguage files

---
+++ resources/lang/en/home.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'title' => 'Wedding and Event Photographer Anastasia Kabakova',
+    'lang' => 'Language',
+    'services' => 'Services',
+    'contacts' => 'Contacts',
+    'presentation' => 'Ciao! sono Anastasia: fotografa e viaggiatrice instancabile. Faccio scatti insoliti , cerco di catturare i sentimenti, le emozioni e le sensazioni di un giorno speciale. Uno dei miei compiti principali è cercare di esaltare la bellezza dei dettagli di un momento unico. Dopo anni di collaborazione con agenzie e privati decido di formare uno staff dei migliori collaboratori con cui ho avuto il piacere di lavorare e aprire uno studio diretto da me. I servizi che offro sono:',
+    'weddings' => 'Weddings',
+    'sessions' => 'Individual Shooting',
+    'reportage' => 'Reportage'
+];
