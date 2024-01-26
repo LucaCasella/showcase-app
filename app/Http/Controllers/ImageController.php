661990d960da84347185f0aa35653c502619444f
Luca <filoluca92@gmail.com>
2024-01-26 19:51:02 +0100

    created Image controller model and migration

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ImageController extends Controller
+{
+    public function index()
+    {
+        return view('images.index');
+    }
+
+    public function create()
+    {
+        return view('images.create');
+    }
+}
