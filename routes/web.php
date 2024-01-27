4ce010cea0a545fd725e9587a3427a413dcd67d2
Manuel9703 <97989541+Manuel9703@users.noreply.github.com>
2024-01-27 10:57:26 +0100

    add routes that show logs

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LogsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('app');
 });
+
+Route::get('/logs', [LogsController::class, 'showLogs']);
