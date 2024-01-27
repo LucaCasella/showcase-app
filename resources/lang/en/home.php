83cf1ce8fd600d2c2ce1377108438543609f7003
Manuel9703 <97989541+Manuel9703@users.noreply.github.com>
2024-01-27 15:25:15 +0100

    update file transaltions

--- resources/lang/en/home.php
+++ resources/lang/en/home.php
@@ -5,7 +5,7 @@ return [
     'lang' => 'Language',
     'services' => 'Services',
     'contacts' => 'Contacts',
-    'presentation' => 'Ciao! sono Anastasia: fotografa e viaggiatrice instancabile. Faccio scatti insoliti , cerco di catturare i sentimenti, le emozioni e le sensazioni di un giorno speciale. Uno dei miei compiti principali è cercare di esaltare la bellezza dei dettagli di un momento unico. Dopo anni di collaborazione con agenzie e privati decido di formare uno staff dei migliori collaboratori con cui ho avuto il piacere di lavorare e aprire uno studio diretto da me. I servizi che offro sono:',
+    'presentation' => "Hello! I'm Anastasia, a photographer and an avid traveler. I capture unique shots, aiming to convey the feelings, emotions, and sensations of a special day. One of my main tasks is to highlight the beauty of the details in a unique moment. After years of collaboration with agencies and individuals, I decided to form a team with the best collaborators I've had the pleasure to work with and open my own studio. The services I offer include:",
     'weddings' => 'Weddings',
     'sessions' => 'Individual Shooting',
     'reportage' => 'Reportage'
