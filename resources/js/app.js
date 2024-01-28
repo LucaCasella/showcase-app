602df6eeded24ef5a0e299db89ad46f9c24090b2
Manuel9703 <97989541+Manuel9703@users.noreply.github.com>
2024-01-28 12:01:16 +0100

    update vite build

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,9 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
+import '../sass/public.scss';
+import '../sass/backoffice.scss';
+
 window.Alpine = Alpine;
 
 Alpine.start();
--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,13 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.scss',
+                'resources/sass/backoffice.scss',
+                'resources/sass/public.scss',
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
             refresh: true,
+            watch: ['resources/**/*.scss'],
         }),
     ],
 });
