1a78c332bc2e0c984621752403499da568086feb
Luca <filoluca92@gmail.com>
2024-01-28 20:08:19 +0100

    updated views

--- resources/views/backoffice/dashboard.blade.php
+++ resources/views/backoffice/dashboard.blade.php
@@ -7,18 +7,28 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mb-5">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    @yield('content')
-                    @include('backoffice.aside')
-                    @include('backoffice.adm-gallery')
+                    hompage backoffice
                 </div>
             </div>
         </div>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-5">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    Website visit count:
+                </div>
+                <div class="website-counter"></div>
+            </div>
+        </div>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-5">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    Information Requests
+                </div>
+                <div class="website-counter"></div>
+            </div>
+        </div>
     </div>
+
 </x-app-layout>
