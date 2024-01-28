0983e538d4f38a813bab53fe9e33f0bd1def5228
Luca <filoluca92@gmail.com>
2024-01-28 20:09:50 +0100

    default title

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Fotografa Matrimoni e Eventi Anastasia Kabakova'),
 
     /*
     |--------------------------------------------------------------------------
