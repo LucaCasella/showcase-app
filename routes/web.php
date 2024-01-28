3caa49f318db60c2a6cdead027e53e8ac51e709e
Luca <filoluca92@gmail.com>
2024-01-28 20:11:00 +0100

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AlbumController;
 use App\Http\Controllers\GuestFormController;
 use App\Http\Controllers\ImageController;
 use App\Http\Controllers\LogController;
@@ -26,13 +27,7 @@ Route::get('/backoffice', function () {
     return view('backoffice.dashboard');
 })->middleware(['auth', 'verified'])->name('backoffice');
 
-Route::get('/adm-gallery', function () {
-    return view('backoffice.dashboard');
-})->middleware(['auth', 'verified'])->name('adm-gallery');
 
-Route::get('/adm-pricing', function () {
-    return view('backoffice.dashboard');
-})->middleware(['auth', 'verified'])->name('adm-pricing');
 
 Route::post('/guest-form-submit',[GuestFormController::class, 'store'])->name('guest-form');
 
@@ -40,6 +35,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/adm-gallery', [AlbumController::class, 'index'])->name('adm-gallery');
+    Route::get('/show-album/{album_id}', [AlbumController::class, 'show']);
+    Route::post('/add-album', [AlbumController::class, 'create']);
+    Route::post('/update-album/{album_id}', [AlbumController::class, 'edit']);
+    Route::post('/delete-album/{album_id}', [AlbumController::class, 'destroy']);
+
+    Route::get('/adm-pricing', function () {
+        return view('backoffice.adm-pricing');
+    })->name('adm-pricing');
 });
 
 Route::middleware('guestVerified')->group(function (){
