18da679c7c4671e1d90c549ee04e9443996e347e
Luca <filoluca92@gmail.com>
2024-01-28 20:10:10 +0100

    created album and image models

---
+++ app/Models/Album.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Album extends Model
+{
+    use HasFactory;
+
+    const ID = 'album_id';
+    const NAME = 'name';
+    const VISIBLE = 'visible';
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+    const DELETED_AT = 'deleted_at';
+
+    protected $primaryKey = Album::ID;
+
+    protected $table = 'albums';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        Album::NAME
+    ];
+
+    protected $hidden = [
+        Album::VISIBLE,
+        Album::CREATED_AT,
+        Album::UPDATED_AT,
+        Album::DELETED_AT
+    ];
+
+    public function image(): HasMany
+    {
+        return $this->hasMany(Image::class, 'image_id', 'album_id');
+    }
+}
--- app/Models/Image.php
+++ app/Models/Image.php
@@ -9,9 +9,36 @@ class Image extends Model
 {
     use HasFactory;
 
+    const ID = 'image_id';
+    const ALBUM_ID = 'album_id';
+    const NAME = 'name';
+    const IMAGE_PATH = 'image_path';
+    const COVER = 'cover';
+    const CREATED_AT = 'image_path';
+    const UPDATED_AT = 'updated_at';
+    const DELETED_AT = 'deleted_at';
+
+    protected $primaryKey = Image::ID;
+
+    protected $table = 'images';
+
+    public $timestamps = false;
+
     protected $fillable = [
         'name',
-        'image',
-        'cover'
     ];
+
+    protected $hidden = [
+        Image::ALBUM_ID,
+        Image::IMAGE_PATH,
+        Image::COVER,
+        Image::CREATED_AT,
+        Image::UPDATED_AT,
+        Image::DELETED_AT
+    ];
+
+    public function album()
+    {
+        return $this->belongsTo(Album::class, 'album_id', 'image_id');
+    }
 }
