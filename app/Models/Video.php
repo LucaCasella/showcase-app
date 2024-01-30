33cd72cd2e30a5ec1b19a95e6ada40857c7c2e54
Luca <filoluca92@gmail.com>
2024-01-30 20:41:04 +0100

    created video model controller and migration

---
+++ app/Models/Video.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Video extends Model
+{
+    use HasFactory;
+
+    const ID = 'video_id';
+    const ALBUM_ID = 'album_id';
+    const NAME = 'name';
+    const VIDEO_PATH = 'video_path';
+    const CREATED_AT = 'created_path';
+    const UPDATED_AT = 'updated_at';
+    const DELETED_AT = 'deleted_at';
+
+    protected $primaryKey = Video::ID;
+
+    protected $table = 'videos';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        Video::NAME,
+    ];
+
+    protected $hidden = [
+        Video::ALBUM_ID,
+        Video::VIDEO_PATH,
+        Video::CREATED_AT,
+        Video::UPDATED_AT,
+        Video::DELETED_AT
+    ];
+
+    public function album()
+    {
+        return $this->belongsTo(Album::class, 'album_id', 'video_id');
+    }
+}
---
+++ database/migrations/2024_01_30_192744_create_videos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id('video_id');
+            $table->foreign('album_id')->references('album_id')->on('albums');
+            $table->string('name');
+            $table->string('video_path');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+};
