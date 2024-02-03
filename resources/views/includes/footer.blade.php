9ee8c6a6e0e9602cad4f3eb90e19deb3d050acf1
Manuel9703 <97989541+Manuel9703@users.noreply.github.com>
2024-02-03 17:54:32 +0100

    fix anchoring contacts

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,6 @@
-<h6 class="d-flex justify-content-center">@lang('home.contacts')</h6>
-<div class="d-flex justify-content-center"><b>@lang('home.number-contact')</b> xxx xxxxxxx</div>
-<div class="d-flex justify-content-center"><b>@lang('home.residence')</b> IMOLA</div>
-<div class="d-flex justify-content-center"><b>@lang('home.vat')</b> xxxxxxxxxxx</div>
+<div id="contacts">
+    <h6 class="d-flex justify-content-center">@lang('home.contacts')</h6>
+    <div class="d-flex justify-content-center"><b>@lang('home.number-contact')</b> xxx xxxxxxx</div>
+    <div class="d-flex justify-content-center"><b>@lang('home.residence')</b> IMOLA</div>
+    <div class="d-flex justify-content-center"><b>@lang('home.vat')</b> xxxxxxxxxxx</div>
+</div>
