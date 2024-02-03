fcec60fb1a8564bf7a94f6d2936b9cfc69b581c2
Manuel9703 <97989541+Manuel9703@users.noreply.github.com>
2024-02-03 16:32:44 +0100

    add translation for whatsapp link

--- resources/lang/en/home.php
+++ resources/lang/en/home.php
@@ -9,5 +9,6 @@ return [
     'presentation' => "Hello! I'm Anastasia, a photographer and an avid traveler. I capture unique shots, aiming to convey the feelings, emotions, and sensations of a special day. One of my main tasks is to highlight the beauty of the details in a unique moment. After years of collaboration with agencies and individuals, I decided to form a team with the best collaborators I've had the pleasure to work with and open my own studio. The services I offer include:",
     'weddings' => 'Weddings',
     'sessions' => 'Individual Shooting',
-    'reportage' => 'Reportage'
+    'reportage' => 'Reportage',
+    'contact-me' => 'Contact me'
 ];
