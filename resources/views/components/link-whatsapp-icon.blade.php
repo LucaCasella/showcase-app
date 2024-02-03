56e0e4bd576794f0d617bda77ff571d18cb633f7
Manuel9703 <97989541+Manuel9703@users.noreply.github.com>
2024-02-03 16:33:27 +0100

    update style view

--- resources/views/components/link-whatsapp-icon.blade.php
+++ resources/views/components/link-whatsapp-icon.blade.php
@@ -1,6 +1,6 @@
 <div class="whatsapp-link-container">
-    <a href="{{ $linkChat . $chatNumber }}" target="_blank">
-        <div>@lang('home.contact')</div>
+    <a href="{{ $linkChat . $chatNumber }}" class="a-tag-whatsapp" target="_blank">
+        <div>@lang('home.contact-me')</div>
         <img src="{{asset('assets/Digital_Glyph_Green.png')}}" alt="whatsapp-icon" class="whatsapp-icon">
     </a>
 </div>
