06858b1f1a53cfc6fb0d68f0749612ebeff7c34a
Luca <filoluca92@gmail.com>
2024-02-07 21:49:57 +0100

    added and updated album controller methods

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -9,10 +9,13 @@ class AlbumController extends Controller
 {
     public function index()
     {
-        $albums = Album::query()->get()->toJson();
-        $albumss = json_decode($albums);
-//        dd($albumss);
-        return view('backoffice.index', compact('albumss'));
+        //retrive all albums
+        $data = Album::query()->get()->toJson();
+
+        //decode to JSON
+        $albums = json_decode($data);
+
+        return view('backoffice.index', compact('albums'));
     }
 
     public function create()
@@ -35,10 +38,37 @@ class AlbumController extends Controller
         return redirect()->route('index-album')->with('success', 'Album created successfully');
     }
 
-    public function edit($id)
+    public function show(Album $album)
     {
-        $page = 'edit';
-        $data = Album::find($id);
-        return view('backoffice.adm-gallery', compact('data', 'page'));
+        return view('backoffice.show', compact('album'));
+    }
+
+    public function edit(Album $album)
+    {
+        return view('backoffice.edit', compact('album'));
+    }
+
+    public function update(Request $request, Album $album)
+    {
+        //validate the input
+        $request->validate([
+            'title' => 'required',
+            'cover_path' => 'required'
+        ]);
+
+        //update the album
+        $album->update($request->all());
+
+        //redirect the user and send friendly message
+        return redirect()->route('index-album')->with('success', 'Album updated successfully');
+    }
+
+    public function destroy(Album $album)
+    {
+        //delete the album
+        $album->delete();
+
+        //redirect the user and display success message
+        return redirect()->route('index-album')->with('success', 'Album deleted successfully');
     }
 }
