e6646405513f462f600ba161a56940a30a6ddd57
Luca <filoluca92@gmail.com>
2024-02-13 21:32:34 +0100

    updated styles

--- resources/views/public.blade.php
+++ resources/views/public.blade.php
@@ -3,17 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>@lang('home.title')</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
-          integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
-          crossorigin="anonymous" referrerpolicy="no-referrer"/>
-
     <link rel="icon" href="{{asset('favicon.ico')}}">
-
     @vite(['resources/js/app.js'])
 </head>
 <body class="container">
@@ -21,7 +12,7 @@
     @include('includes.header')
 </header>
 <main class="main">
-    @include('includes.home')
+    @yield('content')
 </main>
 <footer class="d-flex-column">
     @include('includes.footer')
