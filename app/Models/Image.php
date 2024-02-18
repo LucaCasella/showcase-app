8e10a98d1bef31da419f491de52b2e5bc67e3001
Luca <filoluca92@gmail.com>
2024-02-18 21:11:10 +0100

    minor

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -14,7 +14,7 @@ class Image extends Model
     const NAME = 'name';
     const IMAGE_PATH = 'image_path';
     const COVER = 'cover';
-    const CREATED_AT = 'created_path';
+    const CREATED_AT = 'created_at';
     const UPDATED_AT = 'updated_at';
     const DELETED_AT = 'deleted_at';
 
