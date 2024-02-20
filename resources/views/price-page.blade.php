3261e6bc2864e2ffc498bfecd4cf626fd1203e5f
Luca <filoluca92@gmail.com>
2024-02-20 23:10:56 +0100

    minor

--- resources/views/price-page.blade.php
+++ resources/views/price-page.blade.php
@@ -1,29 +1,28 @@
+<div class="container">
+    <h1>PUBLIC PRICE PAGE</h1>
 
+    <h1>Prezzi e Piani</h1>
 
-    <div class="container">
-        <h1>Prezzi e Piani</h1>
+    <div class="pricing-table">
+        <div class="plan">
+            <h2>Piano Base</h2>
+            <p>Descrizione del piano base...</p>
+            <span class="price">$9.99/mese</span>
+            <a href="#" class="btn">Seleziona</a>
+        </div>
 
-        <div class="pricing-table">
-            <div class="plan">
-                <h2>Piano Base</h2>
-                <p>Descrizione del piano base...</p>
-                <span class="price">$9.99/mese</span>
-                <a href="#" class="btn">Seleziona</a>
-            </div>
+        <div class="plan">
+            <h2>Piano Premium</h2>
+            <p>Descrizione del piano premium...</p>
+            <span class="price">$19.99/mese</span>
+            <a href="#" class="btn">Seleziona</a>
+        </div>
 
-            <div class="plan">
-                <h2>Piano Premium</h2>
-                <p>Descrizione del piano premium...</p>
-                <span class="price">$19.99/mese</span>
-                <a href="#" class="btn">Seleziona</a>
-            </div>
-
-            <div class="plan">
-                <h2>Piano Pro</h2>
-                <p>Descrizione del piano pro...</p>
-                <span class="price">$29.99/mese</span>
-                <a href="#" class="btn">Seleziona</a>
-            </div>
+        <div class="plan">
+            <h2>Piano Pro</h2>
+            <p>Descrizione del piano pro...</p>
+            <span class="price">$29.99/mese</span>
+            <a href="#" class="btn">Seleziona</a>
         </div>
     </div>
-
+</div>
