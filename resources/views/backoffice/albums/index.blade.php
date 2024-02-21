8a1c56a7fbd9c3a323171bc98328146a877218c6
Luca <filoluca92@gmail.com>
2024-02-21 22:34:00 +0100

    fixed album cover render

--- resources/views/backoffice/albums/index.blade.php
+++ resources/views/backoffice/albums/index.blade.php
@@ -29,8 +29,7 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100 flex justify-between">
                     <div>
                         <div>{{$album->title}}</div>
-{{--                        @dd($album->cover_path)--}}
-                        <img class="album-image" src="storage/album_cover/{{$album->cover_path}}" alt="{{$album->title}}">
+                        <img class="album-cover" src="storage/album_covers/{{$album->cover_path}}" alt="{{$album->title}}">
                     </div>
                     <div>
                         <form action="{{route('destroy-album', [$album->album_id])}}" method="post">
