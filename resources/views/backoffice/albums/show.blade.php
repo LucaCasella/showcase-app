f34bcba9c374e937224b63aa16333e2946a51398
Luca <filoluca92@gmail.com>
2024-02-25 17:13:32 +0100

    refactor image to photo 2

--- resources/views/backoffice/albums/show.blade.php
+++ resources/views/backoffice/albums/show.blade.php
@@ -11,7 +11,7 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100 flex justify-between items-center">
                     <h1><strong>{{$album->title}}</strong></h1>
                     <a class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-                       href="{{route('create-image', [$album->album_id])}}">Add Images</a>
+                       href="{{route('create-photo', [$album->album_id])}}">Add Images</a>
                     <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                        href="{{route('index-album')}}">Back</a>
                 </div>
@@ -28,9 +28,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-5">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100 flex justify-between items-center">
-                    @foreach ($images as $image)
-                        <img src="" alt="{{$image->name}}">
+
+                    @foreach ($photos as $photo)
+                        <img class="photo" src="storage/photos/{{$photo->album_id}}/{{$photo->photo}}" alt="{{$photo->name}}">
                     @endforeach
+{{--                        @dd($photos)--}}
                 </div>
             </div>
         </div>
