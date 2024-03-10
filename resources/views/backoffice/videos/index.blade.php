9afddb253df6c0e7cb14897561d36ab09c7577df
Luca <filoluca92@gmail.com>
2024-03-10 17:12:11 +0100

    refactored video index view

--- resources/views/backoffice/videos/index.blade.php
+++ resources/views/backoffice/videos/index.blade.php
@@ -23,32 +23,34 @@
         </div>
     @endif
 
-    @foreach ($videos as $video)
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-5">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100 flex justify-between">
-                    <div>
-                        <div>{{$video->title}}</div>
-                        <video class="video" width="640" height="360" controls controlsList="nodownload">
-                            <source src="storage/videos/{{$video->video}}" type="video/mp4">
-                            Your browser does not support the video tag.
-                        </video>
-                    </div>
-                    <div>
-                        <form action="{{route('destroy-video', [$video->id])}}" method="post">
-                            <a class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
-                               href="{{route('edit-video', [$video->id])}}">Edit</a>
-                            @csrf
-                            @method('DELETE')
-                            <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                                    type="submit">
-                                Delete
-                            </button>
-                        </form>
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-5 adm-videos-container">
+        @foreach ($videos as $video)
+            <div class="max-w-7xl mx-auto video-container">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        <div class="video-info">
+                            <h1 class="video-title">{{$video->title}}</h1>
+                            <form action="{{route('destroy-video', [$video->id])}}" method="post" class="video-form">
+                                <a class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
+                                   href="{{route('edit-video', [$video->id])}}">Edit</a>
+                                @csrf
+                                @method('DELETE')
+                                <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                                        type="submit">
+                                    Delete
+                                </button>
+                            </form>
+                        </div>
+                        <div class="video-wrapper">
+                            <video class="video" width="640" height="360" controls controlsList="nodownload">
+                                <source src="storage/videos/{{$video->video}}" type="video/mp4">
+                                Your browser does not support the video tag.
+                            </video>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    @endforeach
+        @endforeach
+    </div>
 
 </x-app-layout>
