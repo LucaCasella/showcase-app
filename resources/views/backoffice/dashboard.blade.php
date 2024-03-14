823609678735b25bf0cc7fb737bdb21afa0aef8c
Luca <filoluca92@gmail.com>
2024-03-14 21:17:51 +0100

    moved register route in backoffice tab

--- resources/views/backoffice/dashboard.blade.php
+++ resources/views/backoffice/dashboard.blade.php
@@ -1,8 +1,11 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Backoffice') }}
-        </h2>
+        <div class="d-flex flex-row justify-content-between">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ __('Backoffice') }}
+            </h2>
+            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register new Admin</a>
+        </div>
     </x-slot>
 
     <div class="py-12">
