5803cddc1da58a4e445a2a5c7786693b735e3a6d
Luca <filoluca92@gmail.com>
2024-03-19 21:33:36 +0100

    added translations

--- resources/lang/it/home.php
+++ resources/lang/it/home.php
@@ -7,15 +7,24 @@ return [
     'photos' => 'Foto',
     'videos' => 'Video',
     'contacts' => 'Contatti',
-
-    'presentation' => 'Ciao! sono Anastasia: fotografa e viaggiatrice instancabile. Faccio scatti insoliti, cerco di catturare i sentimenti, le emozioni e le sensazioni di un giorno speciale. Uno dei miei compiti principali è cercare di esaltare la bellezza dei dettagli di un momento unico. Dopo anni di collaborazione con agenzie e privati decido di formare uno staff dei migliori collaboratori con cui ho avuto il piacere di lavorare e aprire uno studio diretto da me. I servizi che offro sono:',
-
+    'presentation' => 'Mi chiamo Anastasia Kabakova, dopo anni di lavoro nel settore fotografico insieme al mio compagno Matteo decidiamo di aprire uno studio nostro .
+                        Ad oggi contiamo su una squadra tra fotografi e videomaker, formata dai migliori collaboratori con cui abbiamo avuto il piacere di lavorare .
+                        Innovazione, ricerca e sviluppo sono alla base del nostro metodo di lavoro.
+                        I nostri parchi macchine non raggiungono mai i tre anni di età, per noi è fondamentale rimanere sempre aggiornati in termini di qualità fotografica, video e attrezzatura.
+                        Con noi si inizia un percorso, dove ogni pacchetto viene cucito su misura del cliente, attenzione e cura elle richieste ed esigenze di esso sono capi saldi del nostro modo di lavorare.
+                        <br>Abbiamo studi su: MODENA - BOLOGNA - IMOLA - FORLI',
     'photos-title' => 'Foto',
-    'photos-description' => '',
-
+    'photos-description' => 'Studio e passione per questo lavoro ci hanno portato realizzare fotografie di alto livello .
+                                Per noi è fondamentale capire con il cliente il tipo di prodotto che vuole realizzare personalizzando il nostro stile per ogni tipo di evento.
+                                Ogni nostro scatto viene restituito alla miglior qualità possibile presente sul mercato.
+                                Ogni prima camera fotografa con due fotocamere per risparmiare tempo sul cambio obiettivo così ottenendo un numero maggiore di scatti.',
     'videos-title' => 'Video',
-    'videos-description' => '',
-
+    'videos-description' => 'Ogni nostro video viene pensato per trasmettere emozioni uniche .
+                                Le nostre camere ci permettono di girare in 4k fino a 120fps.
+                                Grazie al formato a 10 bit 4:2:2 possiamo fornire una gamma cromatica unica personalizzabile in post produzione secondo le linee guida del progetto che stiamo svolgendo.
+                                Per avere una qualità audio eccellente siamo forniti sia di microfoni wireless sia ambientali .
+                                Formazione e componenti tecniche all avanguardia ci consentono di andare incontro a tutte le esigenze dei nostri clienti.
+                                Il nostro punto di forza è l uso del drone che ci permette riprese aree mozzafiato.',
     'contact-me' => 'Contattami',
     'services' => 'Scopri gli altri servizi',
     'review' => 'Recensioni',
