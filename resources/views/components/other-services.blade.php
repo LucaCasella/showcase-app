fea1993956da983f23f6dafda9a4a290af06057e
Luca <filoluca92@gmail.com>
2024-03-23 11:20:42 +0100

    added other services section as component 2

---
+++ resources/views/components/other-services.blade.php
@@ -0,0 +1,21 @@
+<div class="other-services d-flex flex-col justify-content-center my-5">
+    <h2>@lang('home.services')</h2>
+    <div class="services-container">
+        <div>
+            <h4 class="service-title">titolo</h4>
+            <img src="immagine1.jpg" alt="Immagine 1">
+        </div>
+        <div>
+            <h4 class="service-title">titolo</h4>
+            <img src="immagine2.jpg" alt="Immagine 2">
+        </div>
+        <div>
+            <h4 class="service-title">titolo</h4>
+            <img src="immagine3.jpg" alt="Immagine 3">
+        </div>
+        <div>
+            <h4 class="service-title">titolo</h4>
+            <img src="immagine4.jpg" alt="Immagine 4">
+        </div>
+    </div>
+</div>
