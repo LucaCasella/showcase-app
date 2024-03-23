b8211ea5dd913e9779c4abde87ccbea3415e2b49
Luca <filoluca92@gmail.com>
2024-03-23 11:20:02 +0100

    updated home buttons and added other services section as component

--- resources/views/includes/services.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="services d-flex justify-content-center my-5">
-    <h2>@lang('home.services')</h2>
-</div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,7 +7,7 @@
                 <img class="image" src="{{asset('assets/AK.jpg')}}" alt="presentation">
             </div>
             <div class="description-wrapper col">
-                <div class="description p-4">
+                <div class="description p-3">
                     <div>
                         <h1 class="">ANASTASIA KABAKOVA</h1>
                     </div>
@@ -19,19 +19,19 @@
         </div>
         <div class="home-container row row-cols-lg-2 row-cols-1">
             <div class="description-wrapper description-revert col">
-                <div class="description p-4">
+                <div class="description p-3">
                     <h1 class="">@lang('home.photos-title')</h1>
                     <div class="lh-lg my-2">
                         @lang('home.photos-description')
                     </div>
-                    <div>
-                        <a class="btn-black" href="#contacts">
-                            @lang('home.goto-prices')
-                        </a>
-                        <a class="btn-white" href="{{route('photos')}}">
-                            @lang('home.goto-albums')
-                        </a>
-                    </div>
+                </div>
+                <div class="link-container my-3">
+                    <a class="link-black" href="#contacts">
+                        @lang('home.goto-prices')
+                    </a>
+                    <a class="link-white" href="{{route('photos')}}">
+                        @lang('home.goto-albums')
+                    </a>
                 </div>
             </div>
             <div class="image-wrapper image-revert col">
@@ -43,24 +43,24 @@
                 <img class="image" src="{{asset('assets/videohp.jpg')}}" alt="videos">
             </div>
             <div class="description-wrapper col">
-                <div class="description p-4">
+                <div class="description p-3">
                     <h1 class="">@lang('home.videos-title')</h1>
                     <div class="lh-lg my-2">
                         @lang('home.videos-description')
                     </div>
-                    <div>
-                        <a class="btn-black" href="#contacts">
-                            @lang('home.goto-prices')
-                        </a>
-                        <a class="btn-white" href="{{route('videos')}}">
-                            @lang('home.goto-videos')
-                        </a>
-                    </div>
+                </div>
+                <div class="link-container my-3">
+                    <a class="link-black" href="#contacts">
+                        @lang('home.goto-prices')
+                    </a>
+                    <a class="link-white" href="{{route('videos')}}">
+                        @lang('home.goto-videos')
+                    </a>
                 </div>
             </div>
         </div>
     </div>
-    @include('includes.services')
+    <x-other-services/>
     <x-reviews/>
     <x-link-matrimonio-site/>
     <x-contact-form/>
