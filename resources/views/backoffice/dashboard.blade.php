6298854bafd2abf60edfb6f4da8d608e198abda7
Luca <filoluca92@gmail.com>
2024-03-30 14:10:01 +0100

    contacts dashboard refactor

--- resources/views/backoffice/dashboard.blade.php
+++ resources/views/backoffice/dashboard.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+
     <x-slot name="header">
         <div class="d-flex flex-row justify-content-between">
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
@@ -10,53 +11,32 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-2">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h5 class="contacts-title">Information Requests</h5>
-                </div>
-            </div>
-        </div>
         <div class="requests-container max-w-7xl mx-auto sm:px-6 lg:px-8 mb-5">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <h5 class="request-title mt-2">Contacted by</h5>
+                <h5 class="request-title mt-3">Contacted by</h5>
                 <div class="p-6 text-gray-900 dark:text-gray-100 contacted-by">
+
                     @foreach($contacts as $contact)
                         <div class="contact-info">
-                            <div class="contact-line">
-                                <div class="contact-attribute">{{$contact->name}}</div>
-                                <div class="contact-attribute">{{$contact->email}}</div>
-                            </div>
-                            <div class="contact-line">
-                                <div class="contact-attribute">{{$contact->phone}}</div>
-                                <div class="contact-attribute">{{ \Carbon\Carbon::parse($contact->created_at)->format('d/m/Y') }}</div>
-                                <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                                   href="{{route('update-contact', [$contact->id])}}">Move to replied</a>
-                            </div>
+                            <div class="">Full name: {{$contact->name}}</div>
+                            <div class="">Email: {{$contact->email}}</div>
+                            <div class="">Phone: {{$contact->phone}}</div>
+                            <div class="">Date: {{$contact->created_at}}</div>
+                            <div class="">Comment: {{$contact->comment}}</div>
+                            <form action="{{route('destroy-contact', [$contact->id])}}" method="post" class="contact-form">
+                                @csrf
+                                @method('DELETE')
+                                <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                                        type="submit">
+                                    Delete
+                                </button>
+                            </form>
                         </div>
                     @endforeach
-                </div>
-            </div>
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <h5 class="request-title mt-2">Replied to</h5>
-                <div class="p-6 text-gray-900 dark:text-gray-100 contacted-by">
-                    @foreach($contacts as $contact)
-                        <div class="contact-info">
-                            <div class="contact-line">
-                                <div class="contact-attribute">{{$contact->name}}</div>
-                                <div class="contact-attribute">{{$contact->email}}</div>
-                            </div>
-                            <div class="contact-line">
-                                <div class="contact-attribute">{{$contact->phone}}</div>
-                                <div class="contact-attribute">{{ \Carbon\Carbon::parse($contact->created_at)->format('d/m/Y') }}</div>
-                                <label>
-                                    <input type="checkbox">
-                                </label>
-                            </div>
-                        </div>
-                    @endforeach
+
                 </div>
             </div>
         </div>
     </div>
+
 </x-app-layout>
