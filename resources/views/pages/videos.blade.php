896a8079f66504e6c57baddc36131a1864435440
Luca <filoluca92@gmail.com>
2024-04-01 00:14:00 +0200

    minor

--- resources/views/pages/videos.blade.php
+++ resources/views/pages/videos.blade.php
@@ -1,7 +1,7 @@
 @extends('public')
 
 @section('content')
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-5 adm-videos-container">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-5 videos-container">
         @foreach ($videos as $video)
             <div class="max-w-7xl mx-auto video-container">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
