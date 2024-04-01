6b9ca6955ba55d5392016f4913abc6ae57429303
Luca <filoluca92@gmail.com>
2024-04-01 19:33:19 +0200

    responsive videos size and full screen fixed

--- resources/views/pages/videos.blade.php
+++ resources/views/pages/videos.blade.php
@@ -1,21 +1,17 @@
 @extends('public')
 
 @section('content')
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-5 videos-container">
+    <div class="videos-container">
         @foreach ($videos as $video)
-            <div class="max-w-7xl mx-auto video-container">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        <div class="video-info">
-                            <h5 class="video-title">{{$video->title}}</h5>
-                        </div>
-                        <div class="video-wrapper">
-                            <video class="video" width="640" height="360" controls controlsList="nodownload">
-                                <source src="storage/videos/{{$video->video}}" type="video/mp4">
-                                Your browser does not support the video tag.
-                            </video>
-                        </div>
-                    </div>
+            <div class="video-wrapper">
+                <div class="video-info">
+                    <h5 class="video-title">{{$video->title}}</h5>
+                </div>
+                <div class="video">
+                    <video class="" width="100%" height="auto" controls controlsList="nodownload">
+                        <source src="storage/videos/{{$video->video}}" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
                 </div>
             </div>
         @endforeach
