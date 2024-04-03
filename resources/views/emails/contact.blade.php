52f45f0cc1e563fdfe879075cc3cce383ec61ea1
Luca <filoluca92@gmail.com>
2024-04-03 21:53:40 +0200

    updated contact mail

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,9 +1,15 @@
-<div style="font-family: Arial, sans-serif; background-color: #f9f9f9; padding: 20px; border-radius: 10px;">
+<div style="font-family: Arial, sans-serif; background-color: #f9f9f9; padding: 20px; border-radius: 10px; display: flex; flex-direction: column; align-items: center">
     <h2 style="color: #ff6f61; margin-bottom: 10px;">Grazie {{$name}} per averci contattato!</h2>
 
-    <p style="color: #666; font-size: 16px; line-height: 1.6;">Non appena possibile ci metteremo in contatto con te per rispondere alle tue esigenze matrimoniali. Nel frattempo, perché non dai un'occhiata alle ultime tendenze e ispirazioni matrimoniali?</p>
+    <p style="color: #666; font-size: 16px; line-height: 1.6;">Non appena possibile ci metteremo in contatto con te per rispondere alle tue esigenze.</p>
+    <p style="color: #666; font-size: 16px; line-height: 1.6;">Nel frattempo seguici anche su Instagram...</p>
 
-    <a href="https://www.anastasiakabakova.com/" target="_blank" style="display: inline-block; background-color: #ff6f61; color: #fff; text-decoration: none; padding: 10px 20px; border-radius: 5px; margin-top: 20px;">Scopri le nostre ispirazioni matrimoniali</a>
+    <div style="display: flex; flex-direction: column; align-items: center">
+        <div style="color: #666; font-size: 16px; line-height: 1.6;">cliccando qui!</div>
+        <a href="https://www.instagram.com/ayriika/" target="_blank" style="display: inline-block; color: #fff; text-decoration: none; border-radius: 5px;">
+            <img src="{{asset('assets/instagram.jpg')}}" alt="" style="width: 50px; height: 50px">
+        </a>
+    </div>
 
     <p style="color: #666; font-size: 14px; margin-top: 20px;">Grazie,<br>Anastasia Kabakova</p>
 </div>
