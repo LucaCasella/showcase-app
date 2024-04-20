9c9330912d57dd6d9504f690e924027832aea964
Luca <filoluca92@gmail.com>
2024-04-20 11:35:32 +0200

    updated reviews with owner div

--- resources/views/components/reviews.blade.php
+++ resources/views/components/reviews.blade.php
@@ -1,3 +1,1 @@
-<div class="rewiews d-flex justify-content-center my-5">
-    <div class='sk-ww-google-reviews' data-embed-id='25384391'></div>
-</div>
+<div class='sk-ww-google-reviews' data-embed-id='25394429'></div>
