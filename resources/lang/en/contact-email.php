8e3c3d8d5bf3b547fa1255170556e2166ed89586
Luca <filoluca92@gmail.com>
2024-04-28 23:17:35 +0200

    multilanguage contact email

--- resources/lang/en/contact-email.php
+++ resources/lang/en/contact-email.php
@@ -1,5 +1,11 @@
 <?php
 
 return [
-    'email' => 'sono inglese'
+    'text1' => 'Thank you ',
+    'text2' => ' for contacting us!',
+    'text3' => 'We will contact you as soon as possible to meet your needs.',
+    'text4' => 'In the meantime, follow us on Instagram...',
+    'text5' => 'by clicking here!',
+    'text6' => 'Thank you, ',
+    'text7' => 'Anastasia Kabakova',
 ];
--- resources/lang/it/contact-email.php
+++ resources/lang/it/contact-email.php
@@ -1,5 +1,11 @@
 <?php
 
 return [
-    'email' => 'sono italiano'
+    'text1' => 'Grazie ',
+    'text2' => ' per averci contattato!',
+    'text3' => 'Non appena possibile ci metteremo in contatto con te per rispondere alle tue esigenze.',
+    'text4' => 'Nel frattempo seguici anche su Instagram...',
+    'text5' => 'cliccando qui!',
+    'text6' => 'Grazie, ',
+    'text7' => 'Anastasia Kabakova',
 ];
