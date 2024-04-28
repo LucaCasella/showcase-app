29001f8e4c152ccc0273e8f1f938b74e5216676b
Luca <filoluca92@gmail.com>
2024-04-28 17:56:33 +0200

    created other-services and instagram-photos components

--- resources/views/components/instagram-photos.blade.php
+++ resources/views/components/instagram-photos.blade.php
@@ -1,36 +1,68 @@
 <div class="other-services d-flex flex-col justify-content-center my-5">
     <div id="photo-container">
-        <div class="photo">
+        <div class="photo1">
             <img src="" alt="Photo">
             <p class="caption"></p>
         </div>
-        <div class="photo">
+        <div class="photo2">
             <img src="" alt="Photo">
             <p class="caption"></p>
         </div>
-        <div class="photo">
+        <div class="photo3">
             <img src="" alt="Photo">
             <p class="caption"></p>
         </div>
-        <div class="photo">
+        <div class="photo4">
             <img src="" alt="Photo">
             <p class="caption"></p>
         </div>
     </div>
 </div>
+<div id="instagram-feed-demo" class="instagram_feed"></div>
 <script>
-    $.ajax({
-        url: 'https://api.instagram.com/v1/users/self/media/recent/?access_token=YOUR_ACCESS_TOKEN',
-        dataType: 'jsonp',
-        success: function(data) {
-            var latestPhotos = data.data.slice(0, 4);
+    fetch('https://graph.instagram.com/me/media?fields=id,media_type&access_token={{env('INSTAGRAM_ACCESS_TOKEN')}}')
+        .then(function(response) {
+            if (!response.ok) {
+                throw new Error('Errore nella richiesta: ' + response.status);
+            }
+            return response.json();
+        })
+        .then(function(data) {
+            var latestAlbums = data.data.filter(function(album) {
+                return album.media_type !== 'VIDEO';
+            }).slice(0, 4);
 
-            // Cicla su ogni foto e aggiorna gli elementi HTML corrispondenti
-            $('.photo').each(function(index) {
-                var photo = latestPhotos[index];
-                $(this).find('img').attr('src', photo.images.standard_resolution.url);
-                $(this).find('.caption').text(photo.caption ? photo.caption.text : '');
+            console.log(latestAlbums);
+
+            // Cicla su ogni album e aggiorna gli elementi HTML corrispondenti
+            latestAlbums.forEach(function(album, index) {
+                fetch('https://graph.instagram.com/' + album.id + '/children?fields=id,permalink&access_token={{env('INSTAGRAM_ACCESS_TOKEN')}}')
+                    .then(function(response) {
+                        if (!response.ok) {
+                            throw new Error('Errore nella richiesta: ' + response.status);
+                        }
+                        return response.json();
+                    })
+                    .then(function(albumData) {
+                        console.log(albumData.data[0]);
+
+                        var className = '.photo' + (index + 1);
+
+                        // var photoElement = document.querySelectorAll('.photo')[index];
+                        var containerElement = document.getElementById('photo-container');
+                        var photoElement = document.querySelector(className);
+
+                        containerElement.appendChild(photoElement);
+
+                        // Aggiorna gli elementi HTML con le informazioni ottenute dalla chiamata fetch
+                        photoElement.querySelector('img').src = albumData.data[0].permalink; // Assumiamo che la prima foto dell'album sia sufficiente
+                    })
+                    .catch(function(error) {
+                        console.log('Si è verificato un errore durante il recupero delle informazioni sull\'album:', error);
+                    });
             });
-        }
-    });
+        })
+        .catch(function(error) {
+            console.log('Si è verificato un errore:', error);
+        });
 </script>
--- resources/views/components/other-services.blade.php
+++ resources/views/components/other-services.blade.php
@@ -1,21 +1,51 @@
-<div class="other-services d-flex flex-col justify-content-center my-5">
-    <h2>@lang('home.services')</h2>
+<div class="other-services my-5">
+    <div>
+        <h2>@lang('home.services')</h2>
+    </div>
     <div class="services-container">
-        <div>
-            <h4 class="service-title">titolo</h4>
-            <img src="immagine1.jpg" alt="Immagine 1">
+        <div class="service-container">
+            <div class="image-container">
+                <img src="immagine1.jpg" alt=@lang('home.premarital')>
+            </div>
+            <div class="service-text">
+                <h4 class="service-title">@lang('home.premarital')</h4>
+                <div class="service-description">
+                    @lang('home.premarital-desc')
+                </div>
+            </div>
         </div>
-        <div>
-            <h4 class="service-title">titolo</h4>
-            <img src="immagine2.jpg" alt="Immagine 2">
+        <div class="service-container">
+            <div class="image-container">
+                <img src="{{asset('assets/polaroid.jpeg')}}" alt=@lang('home.polaroid')>
+            </div>
+            <div class="service-text">
+                <h4 class="service-title">@lang('home.polaroid')</h4>
+                <div class="service-description">
+                    @lang('home.polaroid-desc')
+                </div>
+            </div>
         </div>
-        <div>
-            <h4 class="service-title">titolo</h4>
-            <img src="immagine3.jpg" alt="Immagine 3">
+        <div class="service-container">
+            <div class="image-container">
+                <img src="immagine3.jpg" alt=@lang('home.video-projection')>
+            </div>
+            <div class="service-text">
+                <h4 class="service-title">@lang('home.video-projection')</h4>
+                <div class="service-description">
+                    @lang('home.video-projection-desc')
+                </div>
+            </div>
         </div>
-        <div>
-            <h4 class="service-title">titolo</h4>
-            <img src="immagine4.jpg" alt="Immagine 4">
+        <div class="service-container">
+            <div class="image-container">
+                <img src="immagine4.jpg" alt=@lang('home.wedding-planner')>
+            </div>
+            <div class="service-text">
+                <h4 class="service-title">@lang('home.wedding-planner')</h4>
+                <div class="service-description">
+                    @lang('home.wedding-planner-desc')
+                </div>
+            </div>
         </div>
     </div>
 </div>
