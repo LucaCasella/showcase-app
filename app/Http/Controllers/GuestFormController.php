748920c727ea80822c9e5145ba304d9d59ffd742
Manuel9703 <97989541+Manuel9703@users.noreply.github.com>
2024-05-09 00:00:50 +0200

    Update GuestFormController.php

--- app/Http/Controllers/GuestFormController.php
+++ app/Http/Controllers/GuestFormController.php
@@ -32,7 +32,7 @@ class GuestFormController extends Controller
             'privacy_accepted' => 1
         ])->save;
 
-        Mail::to('mailtrap.club@gmail.com')->send(new Notification($request->name, $request->email, $request->phone, $request->comment));
+        Mail::to('rasoamanuel1@gmail.com')->send(new Notification($request->name, $request->email, $request->phone, $request->comment));
 
         Mail::to($request->input('email'))->send(new ContactMail($request->name));
 
