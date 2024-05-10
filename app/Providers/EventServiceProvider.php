9294fc786440307c7875b26517391c679d7dc621
Manuel9703 <97989541+Manuel9703@users.noreply.github.com>
2024-05-10 20:48:59 +0200

    create event storage link

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace App\Providers;
 
+use App\Events\StorageLinkEvent;
+use App\Listeners\StorageLinkEventListener;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
+
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,6 +20,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        StorageLinkEvent::class =>[
+            StorageLinkEventListener::class
+        ]
     ];
 
     /**
