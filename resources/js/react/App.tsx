3c92c8a06642bf2396f516e7e7a2545db06796dd
Manuel <rasomanuel1@gmail.com>
2025-01-08 22:49:29 +0100

    add React router

--- resources/js/react/App.tsx
+++ resources/js/react/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Counter from "./components/Counter";
 import TestAPI from "./components/TestAPI";
 function App() {
 
     return (
         <>
-            <Counter></Counter>
-            <TestAPI></TestAPI>
+            <Router>
+                <Routes>
+                <Route path="/home" element={<Counter/>}/>
+                <Route path="/album" element={<TestAPI/>}/>
+                </Routes>
+            </Router>
         </>
     );
 }
