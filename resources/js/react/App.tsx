846a3918dfa5e099fdf2587e2a638817e5855520
Manuel <rasomanuel1@gmail.com>
2025-01-08 22:24:38 +0100

    add component TEST API

--- resources/js/react/App.tsx
+++ resources/js/react/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Counter from "./components/Counter";
+import TestAPI from "./components/TestAPI";
 function App() {
 
     return (
         <>
             <Counter></Counter>
+            <TestAPI></TestAPI>
         </>
     );
 }
