49fb120591ef4bb42a22f6e3c31b4a25b2762628
Manuel <rasomanuel1@gmail.com>
2025-01-16 23:26:10 +0100

    create table collaborations

---
+++ database/migrations/2025_01_16_222216_create_collaborations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('collaborations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->boolean('privacy_accepted')->nullable();
+            $table->string('curriculum')->nullable();
+            $table->boolean('replied')->default(0);
+            $table->boolean('visible')->default(1);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('collaborations');
+    }
+};
