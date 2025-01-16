ce1214f607f7996407709de941f938b18e8c218f
Manuel <rasomanuel1@gmail.com>
2025-01-16 21:58:08 +0100

    update with protection route back office work-in-progress

--- resources/js/react/App.tsx
+++ resources/js/react/App.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Counter from "./components/Counter";
+import Login from "./components/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
 import TestAPI from "./components/TestAPI";
+import Counter from "./components/Counter";
+import Logout from "./components/Logout";
 function App() {
 
     return (
         <>
             <Router>
                 <Routes>
-                <Route path="/home" element={<Counter/>}/>
-                <Route path="/album" element={<TestAPI/>}/>
+                <Route path="/login2" element={<Login/>}/>
+                    <Route
+                        path="/dashboard"
+                        element={
+                            <ProtectedRoute>
+                                <TestAPI />
+                                <Logout></Logout>
+                            </ProtectedRoute>
+                        }
+                    />
+                <Route path="/counter" element={<Counter/>}/>
                 </Routes>
             </Router>
         </>
