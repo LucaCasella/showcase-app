d8613cc4730ca36d6e0b0de8119a777de56a9e3e
Manuel <rasomanuel1@gmail.com>
2025-01-16 21:56:49 +0100

    provisional route to test api work in progress

--- resources/js/react/components/TestAPI.tsx
+++ resources/js/react/components/TestAPI.tsx
@@ -1,50 +1,57 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import axiosInstance from "../api/axios";
 
+type User = {
+    name: string;
+    email: string;
+};
 
-const  TestAPI = () => {
-    const [videoIds, setVideoIds] = useState<{
-        lastName: string;
-        firstName: string;
-    }>();
-    const [error, setError] = useState(null);
+const TestAPI = () => {
+    const [user, setUser] = useState<User[]>([]);
+    const [error, setError] = useState<string | null>(null);
 
-    const fetchVideoList = async () => {
+    const fetchUserList = async () => {
         try {
-            const response = await fetch("http://localhost:8000/api/test"); // URL API
-            console.log("Chiamata API inviata...");
-
-            if (!response.ok) {
-                throw new Error(`Errore HTTP! Status: ${response.status}`);
+            const response = await axiosInstance.get('/random-data');
+            console.log(response)
+            if (!response) {
+                throw new Error(`Errore HTTP! Status: ${response}`);
             }
 
-            const data = await response.json(); // Parse della risposta JSON
-            console.log("Risposta API ricevuta:", data);
-            setVideoIds(data); // Imposta i dati nello stato
-        } catch (error) {
-            // @ts-ignore
+            const users = response.data.map((user: any) => ({
+                name: user.name,
+                email: user.email,
+            }));
+
+            setUser(users);
+
+        } catch (error: any) {
             console.error("Errore durante la chiamata API:", error.message);
-            // @ts-ignore
-            setError(error.message); // Imposta l'errore nello stato
+            setError(error.message);
         }
     };
 
     useEffect(() => {
-        fetchVideoList().then(r => videoIds);
+        fetchUserList().then();
     }, []);
 
-    // Render del componente
+
     if (error) return <p>Error: {error}</p>;
 
     return (
         <div>
-            <h1>Video List</h1>
-            {videoIds ? (
+            <h1>User List</h1>
+            {user.length > 0 ? (
                 <ul>
-                   <li>{videoIds.firstName}</li>
-                    <li>{videoIds.lastName}</li>
+                    {user.map((u, index) => (
+                        <li key={index}>
+                            <p>Name: {u.name}</p>
+                            <p>Email: {u.email}</p>
+                        </li>
+                    ))}
                 </ul>
             ) : (
-                <p>No videos found</p>
+                <p>No users found</p>
             )}
         </div>
     );
