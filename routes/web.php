050f57230734b8ef44d1c4403a64220900453086
Manuel <rasomanuel1@gmail.com>
2025-01-18 19:20:44 +0100

    create route

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('pages.home');
-});
+})->name('home');
 
 Route::get('/work-with-us', function (){
     return view('pages.work-with-us');
