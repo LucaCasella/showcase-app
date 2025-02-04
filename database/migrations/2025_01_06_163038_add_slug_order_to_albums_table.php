2a53b8c60b2ce12d393c9e876a8014b853daa42b
manuel <rasomanuel1@gmail.com>
2025-02-04 23:48:23 +0100

    update migrations

---
+++ database/migrations/2025_01_06_163038_add_slug_order_to_albums_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('albums', function (Blueprint $table) {
+            $table->string('slug')->after('id');
+            $table->unsignedInteger('order')->default(1)->after('visible');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('albums', function (Blueprint $table) {
+            $table->dropColumn('slug');
+            $table->dropColumn('order');
+        });
+    }
+};
