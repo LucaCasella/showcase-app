1f425ec04b7c7484a8a340bd1432009ccb26af9d
manuel <rasomanuel1@gmail.com>
2025-02-09 17:36:53 +0100

    remove method from contract

--- services/PhotoManager/PhotoManagerContract.php
+++ services/PhotoManager/PhotoManagerContract.php
@@ -7,6 +7,5 @@ use Illuminate\Http\Request;
 interface PhotoManagerContract
 {
     public function store(Request $request, $album_id);
-    public function update(Request $request, $album_id);
     public function delete(Request $request, $album_id, $photo_id);
 }
