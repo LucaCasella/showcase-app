ce9fb1883dddb065a08daed3de54b9c821944402
Luca <filoluca92@gmail.com>
2025-02-22 19:32:44 +0100

    minor

--- resources/views/react/react.blade.php
+++ resources/views/react/react.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Laravel') }}</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    @vite(['resources/js/app.js', 'resources/css/app.css'])
+    @vite(['resources/js/app.js', 'resources/css/app.css', ])
 </head>
 <body>
 <div id="root">
