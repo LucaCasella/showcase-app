47f21967aee8cf05eb8821438bf60747ad4c4b90
Luca <filoluca92@gmail.com>
2025-02-25 18:48:34 +0100

    updated components

--- resources/js/react/pages/Home.tsx
+++ resources/js/react/pages/Home.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Carousel from "../components/carousel/Carousel";
-import carouselImages from "../__mocks/carousel-images";
 
 const Home = () => {
     return (
-        <div className='text-blue-500 text-7xl'>
-            <Carousel images={carouselImages} />
+        <div className='text-white text-7xl flex flex-col justify-center items-center'>
+            {/*HOME*/}
+            <Carousel />
         </div>
     );
 };
