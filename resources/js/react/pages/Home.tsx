8bf8f582e78b2ef5d478b81658412027ec9f6394
Manuel0301 <rasomanuel1@gmail.com>
2025-03-12 23:57:04 +0100

    fix ci test

--- resources/js/react/pages/Home.tsx
+++ resources/js/react/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Carousel from "../components/carousel/Carousel";
-import GoogleReview from "../components/GoogleReview/GoogleReview";
+
 
 const Home = () => {
     return (
