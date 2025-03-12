a35a80957f9bc11d9925c58e7f954c0ff943c384
Manuel0301 <rasomanuel1@gmail.com>
2025-03-12 23:53:01 +0100

    add config for axios and group string URI

--- resources/js/react/api/axios.tsx
+++ resources/js/react/api/axios.tsx
@@ -1,21 +1,31 @@
 import axios from "axios";
+import { apiUrl } from "../constant/api-url";
+
+
+// @ts-ignore
+const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+
 
 const axiosInstance = axios.create({
-    baseURL: '/api',
+    baseURL: apiUrl.publicUrl.baseUrl + apiUrl.publicUrl.apiPrefix,
     withCredentials: true,
     headers: {
         Accept: 'application/json',
+        'X-Requested-With': 'XMLHttpRequest',
+        'X-CSRF-TOKEN': csrfToken
     }
 });
-axiosInstance.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-}, (error) => {
-    return Promise.reject(error);
-});
+// axiosInstance.interceptors.request.use((config) => {
+//     const token = localStorage.getItem('token');
+//     console.log(token)
+//     if (token) {
+//         config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+// }, (error) => {
+//     return Promise.reject(error);
+// });
 
 export default axiosInstance;
 
---
+++ resources/js/react/constant/api-url.tsx
@@ -0,0 +1,9 @@
+
+export const apiUrl = {
+    "publicUrl": {
+        "baseUrl":"http://localhost:8000/",
+        "apiPrefix": "api/",
+        "googleReview" : "google-review",
+        "submitContact": "submit-contact"
+    }
+}
