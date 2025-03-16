2d2667241468241ecd42ed89c74786bbcc827489
Manuel <rasomanuel1@gmail.com>
2025-03-16 15:50:49 +0100

    eliminate useless features

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,12 +3,6 @@ import './bootstrap';
 import './react/main.tsx'
 import './react/main.css';
 
-import Alpine from 'alpinejs';
-
 import '../css/app.scss';
 import '../sass/main-backoffice.scss'
 import '../sass/main-public.scss';
-
-window.Alpine = Alpine;
-
-Alpine.start();
