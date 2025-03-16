3b37fc654a6ac3c6a239ed567bf523d8e0efefa7
Luca <filoluca92@gmail.com>
2025-03-16 14:50:31 +0100

    updated work with us with translations

---
+++ resources/lang/it/work-with-us.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'Unisciti al nostro Team',
+    'aboutUs' => 'Lo Studio Anastasia Kabakova è una realtà innovativa e digitale del settore matrimoniale. Gli importanti investimenti realizzati negli ultimi anni rispecchiano la crescente attenzione rivolta alla ricerca, all\'innovazione e alla sostenibilità in tutte le sue attività.
+                    Crediamo che persone qualificate e in grado di accogliere le sfide del mercato possano contribuire con successo allo sviluppo dello Studio. Per questa ragione, investiamo nella scelta di persone dinamiche, brillanti, global mindset, con un approccio digitale e aperte al cambiamento.',
+    'subTitle' => 'Chi cerchiamo e perché sceglierci',
+    'who' => 'Ricerchiamo persone con cui condividere la nostra mission e i nostri valori per attuare un progetto di contenuti sempre più emozionanti e tecnologicamente avanzati, anche attraverso iniziative di Employer Branding, eventi di orientamento al lavoro e Recruiting/Job Day, in collaborazione con location del settore.
+            Attenti ai nuovi talenti, valorizziamo le nostre persone promuovendone la crescita nelle aree tecniche, professionali e manageriali, nel rispetto del work-life integration.',
+];
--- resources/views/pages/work-with-us.blade.php
+++ resources/views/pages/work-with-us.blade.php
@@ -1,74 +1,86 @@
 @extends('public')
 @section('content')
- <div>
-     <form action="{{ route('collaborator-form') }}" method="POST" enctype="multipart/form-data">
-         @csrf
+    <div>
+        <h2 class="my-4">@lang('work-with-us.title')</h2>
+        <div class="my-4">
+            <div>
+                @lang('work-with-us.aboutUs')
+            </div>
+            <div>
+                <h5 class="mt-4">
+                    @lang('work-with-us.subTitle')
+                </h5>
+                <p>@lang('work-with-us.who')</p>
+            </div>
+        </div>
+        <form action="{{ route('collaborator-form') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="mb-2">
+                <label for="name" class="form-label">@lang('home.full-name')</label>
+                <input type="text" class="form-control" id="name" aria-describedby="name" name="name" required>
+            </div>
+            <div class="mb-2">
+                <label for="email" class="form-label">@lang('home.email-address')</label>
+                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email" required>
+                <div id="emailHelp" class="form-text">@lang('home.no-share')</div>
+            </div>
+            <div class="mb-2">
+                <label for="phone" class="form-label">@lang('home.number-contact')</label>
+                <input type="tel" class="form-control" id="phone" name="phone" required>
+            </div>
+            <div class="mb-2">
+                <input class="privacycheck mx-2" id="privacycheck" name="privacycheck" type="checkbox" value="false"
+                       required>
+                <label for="privacycheck">@lang('home.privacy-check')</label>
+            </div>
+            <div>
+                <a href="#privacy-modal" class="open-modal mx-3">@lang('home.privacy-info')</a>
+            </div>
+            <div>
+                <label for="pdf">@lang('home.upload-pdf')</label>
+                <input type="file" id="pdf" name="pdf" accept=".pdf, application/pdf">
+            </div>
+            <div class="block mt-4">
+                <div class="g-recaptcha" data-sitekey="{{ config('services.recaptcha_ent.site_key') }}"></div>
+            </div>
+            <div class="submit-container">
+                <button id="contact-submit" type="submit"
+                        class="btn-lg form-submit btn btn-primary my-2">@lang('home.submit')</button>
+            </div>
+        </form>
+        <div id="privacy-modal" class="modal">
+            <div class="modal-content">
+                <span class="close-modal">&times;</span>
+                <h3>@lang('home.privacy-title')</h3>
+                <p>@lang('home.privacy-content')</p>
+            </div>
+        </div>
+    </div>
+    <script>
+        // Apri la modal quando viene cliccato il link
+        document.querySelector('.open-modal').addEventListener('click', function () {
+            document.getElementById('privacy-modal').style.display = 'block';
+        });
 
-         <div class="mb-2">
-             <label for="name" class="form-label">@lang('home.full-name')</label>
-             <input type="text" class="form-control" id="name" aria-describedby="name" name="name" required>
-         </div>
-         <div class="mb-2">
-             <label for="email" class="form-label">@lang('home.email-address')</label>
-             <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email" required>
-             <div id="emailHelp" class="form-text">@lang('home.no-share')</div>
-         </div>
-         <div class="mb-2">
-             <label for="phone" class="form-label">@lang('home.number-contact')</label>
-             <input type="tel" class="form-control" id="phone" name="phone" required>
-         </div>
-         <div class="mb-2">
-             <input class="privacycheck mx-2" id="privacycheck" name="privacycheck" type="checkbox" value="false"
-                    required>
-             <label for="privacycheck">@lang('home.privacy-check')</label>
-         </div>
-             <div>
-                 <a href="#privacy-modal" class="open-modal mx-3">@lang('home.privacy-info')</a>
-             </div>
-         <div>
-             <label for="pdf">@lang('home.upload-pdf')</label>
-             <input type="file" id="pdf" name="pdf" accept=".pdf, application/pdf">
-         </div>
-         <div class="block mt-4">
-             <div class="g-recaptcha" data-sitekey="{{ config('services.recaptcha_ent.site_key') }}"></div>
-         </div>
-         <div class="submit-container">
-             <button id="contact-submit" type="submit" class="btn-lg form-submit btn btn-primary my-2">@lang('home.submit')</button>
-         </div>
-     </form>
-     <div id="privacy-modal" class="modal">
-         <div class="modal-content">
-             <span class="close-modal">&times;</span>
-             <h3>@lang('home.privacy-title')</h3>
-             <p>@lang('home.privacy-content')</p>
-         </div>
-     </div>
- </div>
- <script>
-     // Apri la modal quando viene cliccato il link
-     document.querySelector('.open-modal').addEventListener('click', function() {
-         document.getElementById('privacy-modal').style.display = 'block';
-     });
+        // Chiudi la modal quando viene cliccato il pulsante di chiusura
+        document.querySelector('.close-modal').addEventListener('click', function () {
+            document.getElementById('privacy-modal').style.display = 'none';
+        });
 
-     // Chiudi la modal quando viene cliccato il pulsante di chiusura
-     document.querySelector('.close-modal').addEventListener('click', function() {
-         document.getElementById('privacy-modal').style.display = 'none';
-     });
+        // Chiudi la modal quando viene cliccato al di fuori del contenuto della modal
+        window.addEventListener('click', function (event) {
+            var modal = document.getElementById('privacy-modal');
+            if (event.target === modal) {
+                modal.style.display = 'none';
+            }
+        });
+    </script>
+    <script>
+        const checkbox = document.getElementById("privacycheck");
 
-     // Chiudi la modal quando viene cliccato al di fuori del contenuto della modal
-     window.addEventListener('click', function(event) {
-         var modal = document.getElementById('privacy-modal');
-         if (event.target === modal) {
-             modal.style.display = 'none';
-         }
-     });
- </script>
-<script>
-    const checkbox = document.getElementById("privacycheck");
+        checkbox.addEventListener("change", () => {
 
-    checkbox.addEventListener("change", () => {
-
-        checkbox.setAttribute("value", checkbox.checked ? 1 : 0);
-    });
-</script>
+            checkbox.setAttribute("value", checkbox.checked ? 1 : 0);
+        });
+    </script>
 @endsection
