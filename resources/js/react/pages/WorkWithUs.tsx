a48b51b36b2303a76642a6246d29bf766c393ccf
Manuel0301 <rasomanuel1@gmail.com>
2025-03-24 20:45:08 +0100

    update page work with us with validation submit in work in progress

--- resources/js/react/pages/WorkWithUs.tsx
+++ resources/js/react/pages/WorkWithUs.tsx
@@ -1,13 +1,118 @@
 import React, {useContext, useState} from 'react';
 import {LanguageContext} from "../language_context/LanguageProvider";
+import ReCAPTCHA from "react-google-recaptcha";
+import SuccessNotification from "../components/notificationRequest/SuccessNotification";
+import ErrorNotification from "../components/notificationRequest/ErrorNotification";
+import axiosInstance, {tokenSPAVerify} from "../api/axios";
+import {apiUrl} from "../constant/api-url";
 
 function WorkWithUs() {
     const {languageData} = useContext(LanguageContext);
+    const [successMessage, setSuccessMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
     const [fileName, setFileName] = useState('');
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        comment: "",
+        privacy: false,
+        recaptcha: null,
+        file: null
+    });
 
-    const handleFileChange = (e: any) => {
-        if (e.target.files.length > 0) {
-            setFileName(e.target.files[0].name);
+
+
+    const [errors, setErrors] = useState<{ [key in keyof typeof formData]?: string }>({});
+
+    const validations = {
+        name: (value: string) => !value.trim() ? languageData.contactFormError.name : value.length < 3 ? languageData.contactFormError.hintName: null,
+        email: (value: string) => !value.trim() ? languageData.contactFormError.email : !/^\S+@\S+\.\S+$/.test(value) ? languageData.contactFormError.hintEmail : null,
+        phone: (value: string) => !value.trim() ? languageData.contactFormError.phone : !/^\+?[0-9]{8,15}$/.test(value) ? languageData.contactFormError.hintPhone : null,
+        comment: (value: string) => !value.trim() ? languageData.contactFormError.comment : value.length < 10 ? languageData.contactFormError.hintComment : null,
+        privacy: (value: boolean) => !value ? languageData.contactFormError.privacy : null,
+        recaptcha: (value: string | null) => !value ? languageData.contactFormError.captcha : null,
+        file: (value: File | null) => {
+            if (!value) {
+                return languageData.contactFormError.file;
+            }
+            if (value.type !== "application/pdf") {
+                return languageData.contactFormError.fileType;
+            }
+            if (value.size > 2 * 1024 * 1024) {
+                return languageData.contactFormError.fileSize;
+            }
+            return null;
+        }
+    };
+    const handleChange = (field: keyof typeof formData, value: any) => {
+        setFormData(prev => ({ ...prev, [field]: value }));
+        // @ts-ignore
+        setErrors(prev => ({ ...prev, [field]: validations[field](value) }));
+    };
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files.length > 0) {
+            const file = e.target.files[0];
+
+            handleChange("file", file);
+
+            if (!validations.file(file)) {
+                setFileName(file.name);
+            } else {
+                setFileName('');
+            }
+        }
+    };
+
+
+    const validateForm = () => {
+        let valid = true;
+        const newErrors: typeof errors = {};
+
+        Object.entries(validations).forEach(([field, validate]) => {
+            // @ts-ignore
+            const error = validate(formData[field as keyof typeof formData]);
+            if (error) {
+                newErrors[field as keyof typeof errors] = error;
+                valid = false;
+            }
+        });
+
+        setErrors(newErrors);
+        return valid;
+    };
+
+    const handleSubmit = async (e: React.FormEvent) => {
+
+        e.preventDefault();
+
+        if (validateForm()) {
+            try {
+                const token = await tokenSPAVerify()
+
+                const response = await axiosInstance.post(apiUrl.publicUrl.submitCurriculum, formData, {
+                    headers: {
+                        "Authorization": token,
+                    },
+                });
+
+                if(response.data.success){
+                    setSuccessMessage(languageData.requestNotify.success);
+
+                    setFormData({
+                        name: "",
+                        email: "",
+                        phone: "",
+                        comment: "",
+                        privacy: false,
+                        recaptcha: null,
+                        file: null
+                    });
+                }
+
+            } catch (error: any) {
+                setErrorMessage(languageData.requestNotify.error);
+            }
         }
     };
 
@@ -18,26 +123,38 @@ function WorkWithUs() {
                 <p className='max-w-2xl mx-auto text-md lg:text-xl text-center tracking-widest leading-normal lg:leading-10 font-medium ml-20 my-10'>
                     {languageData.workWithUs.description}
                 </p>
-
+                {successMessage && <SuccessNotification message={successMessage} onClose={() => setSuccessMessage("")}/>}
+                {errorMessage && <ErrorNotification message={errorMessage} onClose={() => setErrorMessage("")}/>}
                 <div className='flex flex-col lg:flex-row gap-10 my-20'>
                     <div className='lg:w-2/3 flex flex-col'>
                         <span className='w-1/4 h-[1px] bg-black'/>
-                        <form action=""
+                        <form onSubmit={handleSubmit}
                               className='flex flex-col gap-16 p-5 border-1 border-y-transparent border-x-black'>
                             <input type="text"
                                    className='text-center border-none border-bottom focus:outline-none focus:ring-0 placeholder:text-black placeholder:text-center placeholder:tracking-widest placeholder:text-lg'
-                                   placeholder={languageData.utils.form.name}/>
+                                   placeholder={languageData.utils.form.name}
+                                   value={formData.name}
+                                   onChange={(e) => handleChange("name", e.target.value)}/>
+                            {errors.name && <p className="text-red-500">{errors.name}</p>}
                             <input type="email"
                                    className='text-center border-none border-bottom focus:outline-none focus:ring-0 placeholder:text-black placeholder:text-center placeholder:tracking-widest placeholder:text-lg'
-                                   placeholder={languageData.utils.form.email}/>
+                                   placeholder={languageData.utils.form.email}
+                                   value={formData.email}
+                                   onChange={(e) => handleChange("email", e.target.value)}/>
+                            {errors.email && <p className="text-red-500">{errors.email}</p>}
                             <input type="tel"
                                    className='text-center border-none border-bottom focus:outline-none focus:ring-0 placeholder:text-black placeholder:text-center placeholder:tracking-widest placeholder:text-lg'
-                                   placeholder={languageData.utils.form.phone}/>
-                            <textarea name="message"
+                                   placeholder={languageData.utils.form.phone}
+                                   value={formData.phone}
+                                   onChange={(e) => handleChange("phone", e.target.value)}/>
+                            {errors.phone && <p className="text-red-500">{errors.phone}</p>}
+                            <textarea name="comment"
                                       id="contact-message"
                                       className='h-32 min-h-20 text-center border-none border-bottom focus:outline-none focus:ring-0 placeholder:text-black placeholder:text-center placeholder:tracking-widest placeholder:text-lg'
                                       placeholder={languageData.utils.form.message}
-                            />
+                                      value={formData.comment}
+                                      onChange={(e) => handleChange("comment", e.target.value)}/>
+                            {errors.comment && <p className="text-red-500">{errors.comment}</p>}
                             <div className="w-full flex flex-col items-center space-x-2 self-center">
                                 <input
                                     id="file-upload"
@@ -52,12 +169,22 @@ function WorkWithUs() {
                                     {languageData.workWithUs.cvUpload}
                                 </label>
                                 {fileName && <span>{fileName}</span>}
+                                {errors.file && <p className="text-red-500 mt-2">{errors.file}</p>}
                             </div>
                             <div className='flex gap-4 items-center'>
-                                <input type="checkbox" className='w-6 h-6'/>
+                                <input type="checkbox"  checked={formData.privacy} className='w-6 h-6'  onChange={(e) => handleChange("privacy", e.target.checked)}
+                                />
                                 <p className='my-auto'>
                                     {languageData.utils.form.privacy1}<a href="">{languageData.utils.form.privacy2}</a>{languageData.utils.form.privacy3}
                                 </p>
+                                {errors.privacy && <p className="text-red-500">{errors.privacy}</p>}
+                            </div>
+                            <div>
+                                <ReCAPTCHA
+                                    sitekey="6LccrpYpAAAAAGcl7WBDiRWSkDNbOgGZvFefjFYb"
+                                    onChange={(value) => handleChange("recaptcha", value)}
+                                />
+                                {errors.recaptcha && <p className="text-red-500">{errors.recaptcha}</p>}
                             </div>
                             <button type='submit' className='w-full md:w-1/2 mx-auto font-normal border p-2'>{languageData.utils.form.submit}
                             </button>
