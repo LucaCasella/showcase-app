33a47898f7f253af763663caba8c8a1f172764e8
Luca <filoluca92@gmail.com>
2025-04-10 21:21:09 +0200

    added whatsapp icon

---
+++ resources/js/react/components/whatsapp/WhatsappIcon.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './whatsapp.scss';
+const WhatsappIcon = () => {
+    return (
+        <div className="whatsapp-link-container">
+            <a href='https://api.whatsapp.com/send?phone=3803797287' className="a-tag-whatsapp" target="_blank">
+                <img src={'assets/Digital_Glyph_Green.png'} alt="whatsapp-icon" className="whatsapp-icon"/>
+            </a>
+        </div>
+    );
+};
+
+export default WhatsappIcon;
