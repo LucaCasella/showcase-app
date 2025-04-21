ca0ffb2c288a21ab775115d2a423a4ff24ebecc6
Manuel0301 <rasomanuel1@gmail.com>
2025-04-21 17:48:34 +0200

    update style payoff

--- resources/js/react/components/Payoff.tsx
+++ resources/js/react/components/Payoff.tsx
@@ -7,7 +7,7 @@ const Payoff = () => {
     return (
         <div className='max-w-min text-nowrap flex flex-col gap-3 lg:gap-20 text-white left-5 lg:left-10 bottom-5 lg:bottom-10'>
             {/* todo: low - add fade in to title */}
-            <p className='italic text-lg lg:text-3xl xl:text-5xl font-semibold tracking-wide'>{languageData.header.payoff.title}</p>
+            <p className='italic text-sm lg:text-3xl xl:text-5xl font-semibold tracking-wide'>{languageData.header.payoff.title}</p>
             <div className='flex gap-10 items-center'>
                 <p className='hidden md:flex text-sm lg:text-xl xl:text-2xl font-semibold m-0'>{languageData.header.payoff.winner}</p>
                 <div className='w-14 h-14 sm:w-20 sm:h-20'>
