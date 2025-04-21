be8a2289ce80b0681f43b40358c3fe6d38e1cc29
Manuel0301 <rasomanuel1@gmail.com>
2025-04-21 17:48:24 +0200

    update style

--- resources/js/react/components/indicator_loading/LoadingIndicator.tsx
+++ resources/js/react/components/indicator_loading/LoadingIndicator.tsx
@@ -5,9 +5,9 @@ import Logo from './loading.png';
 const LoadingIndicator = () => {
     return (
         <>
-            <div className="logo-container">
-                <img src={Logo} alt="Logo" className="logo"/>
-                <div className="logo-fill"></div>
+            <div className="logo-container-1">
+                <img src={Logo} alt="Logo" className="logo-1"/>
+                <div className="logo-fill-1"></div>
             </div>
         </>
     )
