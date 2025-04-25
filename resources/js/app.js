f281472b69ab79e33d265c16ee6511f55f635966
Manuel0301 <rasomanuel1@gmail.com>
2025-04-25 14:49:14 +0200

    rollback

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,6 @@ import './bootstrap';
 import './react/main.tsx'
 import './react/main.css';
 
-import '../css/app.scss';
+import '../css/app.css';
 import '../sass/main-backoffice.scss'
 import '../sass/main-public.scss';
